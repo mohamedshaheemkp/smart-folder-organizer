c36e8377fbc4d52824078a961e07897f46a6322e
shaheem kp <mohamedshaheemkp74@gmail.com>
2026-01-12 23:52:57 +0530

    Day 1: list files in folder

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("Smart Folder Organizer ")
+import os
+fpath = input("Enter the folder path to organize: ")
+if not os.path.exists(fpath):
+    print("The specified folder path does not exist.")
+else:
+    print("Files in folder")
+    for item in os.listdir(fpath):
+        print(item)
