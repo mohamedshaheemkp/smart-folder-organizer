f935c08c46028a179c1e68c720634560547ba50a
shaheem kp <mohamedshaheemkp74@gmail.com>
2026-01-18 16:28:42 +0530

    Day 3: basic organizer with dry run and logging

--- main.py
+++ main.py
@@ -1,34 +1,62 @@
 print("Smart Folder Organizer ")
 import os
+import time
+#log
+def write_log(message):
+    with open("lo.txt", "a") as log:
+         timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+         log.write(f"{timestamp} - {message}\n")
+#i/p
 fpath = input("Enter the folder path to organize: ")
 if not os.path.exists(fpath):
     print("The specified folder path does not exist.")
-else:
-    print("Files in folder")
-    for item in os.listdir(fpath):
-        print(item)
-    extensions = {
+    exit()
+dry_run = input("Do you want to do a dry run? (yes/no): ").strip().lower() == "yes"
+print("Dry run mode:", dry_run)
+
+# file extensions and their corresponding folders
+extensions = {
         'Images': ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff'],
         'Documents': ['.pdf', '.docx', '.txt', '.xlsx', '.pptx'],
         'Audio': ['.mp3', '.wav', '.aac', '.flac'], 
         'Videos': ['.mp4', '.avi', '.mov', '.mkv'],
         'Archives': ['.zip', '.rar', '.tar', '.gz'],
-        'Scripts': ['.py', '.js', '.sh', '.bat']}
-    files = os.listdir(fpath)
-    for files in files:
-        file_path = os.path.join(fpath,files)
-        if os.path.isfile(file_path):
-            moved = False
-        for folder, exts in extensions.items():
-            if files.lower().endswith(tuple(exts)):
-                target_folder = os.path.join(fpath, folder)
-                os.makedirs(target_folder, exist_ok=True)
-                os.rename(file_path, os.path.join(target_folder, files))
-                print(f"Moved: {files} ➡️ {folder} folder")
-                moved = True
-                break
-        if not moved:
-                other_folder = os.path.join(fpath, 'Others')
-                os.makedirs(other_folder, exist_ok=True)
-                os.rename(file_path, os.path.join(other_folder, files))
-                print(f"Moved: {files} ➡️ Others folder")
+        'Scripts': ['.py', '.js', '.sh', '.bat'],
+        'Programs': ['.exe', '.msi', '.dmg']}
+
+# organization logic
+files = os.listdir(fpath)
+
+for file in files:
+    file_path = os.path.join(fpath, file)
+
+    if not os.path.isfile(file_path):
+        continue
+    
+    moved = False
+
+    for folder, exts in extensions.items():
+        if file.lower().endswith(tuple(exts)):
+            if dry_run:
+                 print(f"[DRY RUN] {file} -> {folder}")
+            else:
+                 target_folder = os.path.join(fpath, folder)
+                 os.makedirs(target_folder, exist_ok=True)
+                 os.rename(file_path, os.path.join(target_folder, file))
+                 print(f"Moved: {file} -> {folder} folder")
+                 write_log(f"{file} -> {folder}")
+
+            moved = True
+            break
+
+
+#others folder logic
+    if not moved:
+         if dry_run:
+            print(f"[DRY RUN] {file} -> Others")
+         else:
+            other_folder = os.path.join(fpath, 'Others')
+            os.makedirs(other_folder, exist_ok=True)
+            os.rename(file_path, os.path.join(other_folder, file))
+            print(f"moved {file} -> Others")
+            write_log(f"{file} -> Others")
